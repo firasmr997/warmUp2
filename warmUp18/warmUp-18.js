2fd7b5be0417ac5b79b06d51175f08ae15a32ae5
Mohamed Zemzemi <mzemzemi9@gmail.com>
2020-02-25 10:07:20 +0100

    finish

--- warmUp18/warmUp-18.js
+++ warmUp18/warmUp-18.js
@@ -15,19 +15,39 @@
             L          50
             C          100
             D          500
-            M          1,000 
-    
-    2. Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+            M          1,000
+            */
+    function solution(romanNumber) {
+
+    }
+  /*  2. Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
         Examples:
 
         toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
 
-        toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
-    
-    3. In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+        toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior" */
+        function toCamelCase(string) {
+           
+            var x=string.split(/-|_/);
+            var array=[];
+            for (var i=0; i<x.length; i++) {
+                
+                array.push(x[i].substring(0,1).toUpperCase()+x[i].substring(1))
+                ;}
+                return array.join("")}
+  /*  3. In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
         Example
 
             filter_list([1,2,'a','b']) == [1,2]
             filter_list([1,'a','b',0,15]) == [1,0,15]
             filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 */
+function filter_list(array) {
+    var num=[];
+    for (var i=0; i<array.length; i++) {
+        if (typeof(array[i])==='number') {
+            num.push(array[i])
+        }
+    }
+    return num
+}
